90c0c63125a2484f723914063e805a9e4b3272b2
Dina <dinagiacobbi@gmail.com>
2022-08-26 11:13:20 -0800

    Added Kitten URL and filtering features

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react"
 
-import './App.css';
-import CardList from './components/cardList/cardList.component';
-import SearchBox from './components/searchBox/searchBox.component';
+import "./App.css"
+import CardList from "./components/cardList/cardList.component"
+import SearchBox from "./components/searchBox/searchBox.component"
 
 const App = () => {
-
-  const [kittens, setKittens] = useState([])
   const [searchField, setSearchField] = useState("")
-  const [filteredKittens, setFilteredKittens] = useState(kittens)
+  const [kittens, setKittens] = useState([])
+  const [filteredKittens, setFilteredKittens] = useState([])
 
   useEffect(() => {
-    fetch("https://randomuser.me/api/?results=12")
+    console.log("use effect kitt fires now")
+    fetch("https://api.onlinewebtutorblog.com/employees")
       .then((response) => response.json())
       .then((users) => {
-        setKittens(users)
+        setKittens(users.data)
         console.log(users)
       })
   }, [])
 
   useEffect(() => {
+    console.log("Next use effect to filter")
     const newFilteredKittens = kittens.filter((kitten) => {
       return kitten.name.toLowerCase().includes(searchField)
     })
@@ -35,14 +36,14 @@ const App = () => {
     <div className="App">
       <h1 className="app-title">Cartoon KittenDex</h1>
       <SearchBox
-          className="kitten-search-box" 
-          placeholder="Search Kittens"
-          onChangeHandler={onSearchChange}
-          />
+        className="kitten-search-box"
+        placeholder="Search Kittens"
+        onChangeHandler={onSearchChange}
+      />
 
       <CardList kittens={filteredKittens} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from "react"
 
-const Card = () => {
- return (
-    <div className='card-container'>
-        <img src="" alt="cartoon cats" />
+const Card = ({ kitten }) => {
+  const { name, email } = kitten
+  const id = Math.floor(Math.random() * 100)
 
-        
+  return (
+    <div className="card-container" key={id}>
+      <img
+        src={`https://robohash.org/${id}?set=set4&size=150x150`}
+        alt="cartoon cats"
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
     </div>
-
-
- )
-
+  )
 }
 
 export default Card
